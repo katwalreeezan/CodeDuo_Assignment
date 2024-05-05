b8900df825c1ea02080226c0c9f191a031e6d3ff
unknown <chemankatwal111@gmail.com>
2024-05-05 22:18:23 +0545

    Stored the favoritelist in localstorage

--- src/features/SpellList/SpellLists.tsx
+++ src/features/SpellList/SpellLists.tsx
@@ -1,53 +1,75 @@
 // SpellLists.tsx
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { FaRegHeart } from 'react-icons/fa6';
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { FaHeart, FaRegHeart } from "react-icons/fa6";
 
-
-
-import { RootState } from '../../app/store';
-import { useGetSpellsQuery } from '../../service/api/apiRequest';
-import { addToFavorites, removeFromFavorites } from '../../service/features/favoriteSpellSlice';
-import Loader from '../../components/Loader';
+import { RootState } from "../../app/store";
+import { useGetSpellsQuery } from "../../service/api/apiRequest";
+import {
+  addToFavorites,
+  removeFromFavorites,
+} from "../../service/features/favoriteSpellSlice";
+import Loader from "../../components/Loader";
+import { toast } from "react-toastify";
 
 const SpellLists = () => {
   const dispatch = useDispatch();
   const { data: allSpells, isSuccess, isLoading } = useGetSpellsQuery();
-  const favoriteSpells = useSelector((state: RootState) => state.favoriteSpells.favoriteSpells);
-  console.log(favoriteSpells)
+  const favoriteSpells = useSelector(
+    (state: RootState) => state.favoriteSpells.favoriteSpells
+  );
+  console.log(favoriteSpells);
 
   const handleFavoriteClick = (spell) => {
-    const isFavorite = favoriteSpells.some(favoriteSpell => favoriteSpell.index === spell.index);
-    
+    const isFavorite = favoriteSpells.some(
+      (favoriteSpell) => favoriteSpell.index === spell.index
+    );
+    toast.success(isFavorite ? 'Removed from favorites' : 'Added to favorites');
     if (isFavorite) {
-      dispatch(removeFromFavorites(spell)); // Dispatch action to remove from favorites
-    
+      dispatch(removeFromFavorites(spell)); 
     } else {
-      dispatch(addToFavorites(spell)); // Dispatch action to add to favorites
-    
+      dispatch(addToFavorites(spell)); 
     }
   };
 
   return (
     <div className="container mt-5">
       {isLoading ? (
-      <Loader/>
+        <Loader />
       ) : (
         <>
           <h5 className="fw-bold">List of All Spells</h5>
           <div className="row gy-4">
+            {favoriteSpells.map((item)=><div key={item.index}>{item.name}</div>)}
             {isSuccess &&
               allSpells?.results?.map((item) => (
                 <div className="col-lg-4" key={item.index}>
                   <div className="card shadow-lg">
                     <div className="card-body">
-                      <div className="text-dark fw-bold text-uppercase">{item.name}</div>
+                      <div className="text-dark fw-bold text-uppercase">
+                        {item.name}
+                      </div>
                       <div className="d-flex justify-content-between mt-3">
                         <Link to={`/spell/${item.index}`}>
                           <button className="btn btn-primary">View</button>
                         </Link>
-                        <button className="btn btn-outline-dark fw-bold d-block" onClick={() => handleFavoriteClick(item)}>
-                          Add To Favourite <FaRegHeart />
+
+                        <button
+                          className="btn btn-outline-dark fw-bold d-block"
+                          onClick={() => handleFavoriteClick(item)}
+                        >
+                          {favoriteSpells.some(
+                            (favoriteSpell) =>
+                              favoriteSpell.index === item.index
+                          ) ? (
+                            <>
+                              Remove From Favorites <FaHeart />
+                            </>
+                          ) : (
+                            <>
+                              Add To Favorites <FaRegHeart />
+                            </>
+                          )}
                         </button>
                       </div>
                     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,14 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { Provider } from 'react-redux';
 import { store } from './app/store.ts';
 import { BrowserRouter } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+      <ToastContainer />
     <App />
     </BrowserRouter>
     </Provider>
